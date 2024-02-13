88e270ea149ed4fe60a158dcee16cfc037d79101
Denis Borisovich Fishman <106389581+fishman123456@users.noreply.github.com>
2024-02-13 10:46:19 +0700

    13-02-2023 рабочий вариант

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -17,8 +17,12 @@ namespace Autocad_Wpf_Autolip_Pipe_12_02_2024
     /// </summary>
     public partial class MainWindow : Window
     {
+        // список для значений тексбокса имя слоя
         public List<string> list_lay_name = new List<string>();
+        // список для значений текстбокса диаметры
         public List<string> list_pipe_Diam = new List<string>();
+        StringBuilder sb = new StringBuilder();
+        
         public MainWindow()
         {
             InitializeComponent();
@@ -43,23 +47,47 @@ namespace Autocad_Wpf_Autolip_Pipe_12_02_2024
             // добавляем данные в список из текстбокса TextBox_Lay_name 
             string[] mass_textBox_Lay_name = TextBox_Lay_name.Text.Split(separator, StringSplitOptions.RemoveEmptyEntries);
             // добавляем данные в список из текстбокса TextBox_Block_name
-            string[] mass_block_name = TextBox_Pipe_Diam.Text.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+            string[] mass_diam_cable = TextBox_Pipe_Diam.Text.Split(separator, StringSplitOptions.RemoveEmptyEntries);
             int count = 0;
 
             try
             {
                 foreach (var item in mass_textBox_Lay_name)
-                {
+                {     
+                    list_pipe_Diam.Add(mass_diam_cable[count]);
                     list_lay_name.Add(mass_textBox_Lay_name[count]);
-                    list_pipe_Diam.Add(mass_block_name[count]);
                     count++;
                 }
+                // собираем lisp
+                sb.Append(String_Lisp.begin);
+                sb.Append("\n");
+                // добавляем диаметры из текстбокса
+                foreach (var item in list_pipe_Diam)
+                {
+                    sb.Append(item);
+                    sb.Append("\n");
+                }
+                //sb.Append(String_Lisp.diam); 
+                //sb.Append("\n");
+                sb.Append(String_Lisp.middle);
+                sb.Append("\n");
+                // добавляем имя слоя из текстбокса
+                foreach (var item in list_lay_name)
+                {
+                    sb.Append("\"" + item + "\"");
+                    sb.Append("\n");
+                }
+                //sb.Append(String_Lisp.laystr);
+                //sb.Append("\n");
+                sb.Append(String_Lisp.endstr);
+                sb.Append("\n");
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.ToString());
             }
-            SaveFile.saveFileMetod(String_Lisp.stringBuilder.ToString());
+           
+            SaveFile.saveFileMetod(sb.ToString());
         }
     }
 }
